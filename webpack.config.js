58b567b190a80d84e968a8f3c7501069965e4aec
Samet Gecgel <sgrio1453@gmail.com>
2023-06-01 13:25:25 +0300

    update webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './app.js',
   output: {
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+  },
+  target: 'node',
+  resolve: {
+    fallback: {
+      assert: require.resolve('assert/'),
+      url: require.resolve('url/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
