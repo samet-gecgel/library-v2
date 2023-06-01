dcc089269edb6bcf5b288486e44c7e7d8a201583
Samet Gecgel <sgrio1453@gmail.com>
2023-06-01 18:08:25 +0300

    heroku6

--- data/db.js
+++ data/db.js
@@ -1,25 +1,24 @@
-const config = require("../views/config")
+const config = require("../views/config");
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(config.db.database, config.db.user, config.db.password, {
-    dialect: "mysql",
-    host: config.db.host,
-    define: {
-        timestamps: false
-    },
-    storage: "./session.mysql"
+  dialect: "mysql",
+  host: config.db.host,
+  define: {
+    timestamps: false
+  },
+  storage: "./session.mysql"
 });
 
-  async function connect(){
-    try{
-      await sequelize.authenticate();
-      console.log("mysql server connected");
-    }
-    catch(err){
-      console.log("mysql server connection failed",err);
-    }
+async function connect() {
+  try {
+    await sequelize.authenticate();
+    console.log("mysql server connected");
+  } catch (err) {
+    console.log("mysql server connection failed", err);
   }
+}
 
-  connect();
+connect();
 
-  module.exports = sequelize;
+module.exports = sequelize;
--- views/config.js
+++ views/config.js
@@ -1,16 +1,16 @@
 const config = {
-    db : {
-        connectionLimit: 1,
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'Sifre1453.',
-        database: 'bookdb',
+    db: {
+      connectionLimit: 1,
+      host: '127.0.0.1',
+      user: 'root',
+      password: 'Sifre1453.',
+      database: 'bookdb',
     },
     email: {
-        username : "sametgecgel68@hotmail.com",
-        password : "Sgrio1453.",
-        from : "sametgecgel68@hotmail.com"
+      username: "sametgecgel68@hotmail.com",
+      password: "Sgrio1453.",
+      from: "sametgecgel68@hotmail.com"
     }
   };
-
+  
   module.exports = config;
