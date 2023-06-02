32793e81bc6bbec35a8160100ba396311d68ce0f
Samet Gecgel <sgrio1453@gmail.com>
2023-06-02 15:21:31 +0300

    heroku10

--- views/config.js
+++ views/config.js
@@ -1,10 +1,10 @@
 const config = {
     db: {
       connectionLimit: 1,
-      host: '127.0.0.1',
-      user: 'root',
-      password: 'Sifre1453.',
-      database: 'bookdb',
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
     email: {
       username: "sametgecgel68@hotmail.com",
