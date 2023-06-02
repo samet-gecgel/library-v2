8c26709ad623107fc995d000edfc2052efc7844b
Samet Gecgel <sgrio1453@gmail.com>
2023-06-02 11:52:10 +0300

    heroku7

--- views/config.js
+++ views/config.js
@@ -1,10 +1,10 @@
 const config = {
     db: {
       connectionLimit: 1,
-      host: 'eu-cdbr-west-03.cleardb.net',
-      user: 'b9e87b7d033e0f',
-      password: 'd60bf8a7',
-      database: 'heroku_2ef6f2be8b1966d',
+      host: '127.0.0.1',
+      user: 'root',
+      password: 'Sifre1453.',
+      database: 'bookdb',
     },
     email: {
       username: "sametgecgel68@hotmail.com",
