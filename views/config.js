580746c8d4336dd89778aa6de4378237e540e99d
Samet Gecgel <sgrio1453@gmail.com>
2023-06-05 17:45:58 +0300

    cloud

--- views/config.js
+++ views/config.js
@@ -1,10 +1,10 @@
 const config = {
   db : {
       connectionLimit: 1,
-      host: '127.0.0.1',
-      user: 'root',
-      password: 'Sifre1453.',
-      database: 'bookdb',
+      host: 'bwpu3iu7vxlxpddpbzdg-mysql.services.clever-cloud.com',
+      user: 'uui3nylnhqs5fcjm',
+      password: '1ouI0np0GxdxgBggQ1r9',
+      database: 'bwpu3iu7vxlxpddpbzdg',
       port: 3306
   },
   email: {
