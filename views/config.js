859ec1899ad0744488cb8054bd50129f5467c8c1
Samet Gecgel <sgrio1453@gmail.com>
2023-06-05 17:53:03 +0300

    cloudd

--- views/config.js
+++ views/config.js
@@ -5,7 +5,7 @@ const config = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      port: 3306
+      port: process.env.DB_PORT
   },
   email: {
       username : "sametgecgel68@hotmail.com",
