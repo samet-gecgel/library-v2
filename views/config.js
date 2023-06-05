e7e4c713c03e807f2fee5f1dd49e6149aaf96fe2
Samet Gecgel <sgrio1453@gmail.com>
2023-06-05 16:20:08 +0300

    router update

--- views/config.js
+++ views/config.js
@@ -1,11 +1,11 @@
 const config = {
   db : {
       connectionLimit: 1,
-      host: '127.0.0.1',
-      user: 'root',
-      password: 'Sifre1453.',
-      database: 'bookdb',
-      port: 3306
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT
   },
   email: {
       username : "sametgecgel68@hotmail.com",
